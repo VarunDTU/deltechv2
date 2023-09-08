77bc3dd91657693f6f17e7a5a2fa9add9a0aee6e
rahul-sharma13 <rahulharit13@gmail.com>
2023-09-08 19:55:56 +0530

    members section and tailwind config

--- app/homepage/memberSection.js
+++ app/homepage/memberSection.js
@@ -27,12 +27,13 @@ const Members = () => {
     }
 
     return (
-        <div className='mt-3 max-w-screen'>
+        <div className=' max-w-screen'>
             <div className="relative flex">
                 <div className="top-0 left-[30vw] absolute opacity-60 text-[#E5E5E5] text-9xl font-bold font-urbanist">MEMBERS</div>
                 <div className="top-4 left-[41vw] absolute text-[#262626] text-[64px] font-bold drop-shadow-xl font-urbanist leading-[96px]">Members</div>
             </div>
-            <div className='pt-4 absolute top-[180px] px-6 flex flex-col w-full gap-[50px] justify-center items-center'>
+             
+            <div className='pt-4 px-6 mt-[150px] flex flex-col w-full gap-[50px] justify-center items-center'>
                 <div className='flex flex-wrap gap-20 justify-center '>
                     {members_one.map((member, index) => (
                         <MembersCard key={`member-${index}`} index={1} member={member} />
@@ -51,6 +52,7 @@ const Members = () => {
                     </motion.button>
                 </div>
             </div>
+            
         </div>
     )
 }
