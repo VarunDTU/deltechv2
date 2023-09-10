98eab8d263fbbc96fce6f158358302db56fb193c
Vaibhav <vaibhav2438@gmail.com>
2023-09-10 16:14:10 +0530

    header , blog css changes and registration pages added

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -8,10 +8,9 @@ import { signIn } from "next-auth/react";
 
 const navigation = [
   { name: "Home", href: "/", current: true },
-  { name: "About", href: "#", current: false },
-  { name: "Members", href: "#", current: false },
   { name: "Blogs", href: "/blog", current: false },
-  { name: "Contact", href: "#", current: false },
+  { name: "DelTech MUN'24", href: "/deltech24", current: false },
+  { name: "Campus Ambasador", href: "/campus_ambasador", current: false },
 ];
 
 function classNames(...classes) {
@@ -44,7 +43,7 @@ export default function NewNavbar() {
     >
       {({ open }) => (
         <>
-          <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+          <div className="mx-auto max-w-7xl p-2 sm:px-6 lg:px-8">
             <div className="relative flex h-16 items-center justify-between">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                 {/* Mobile menu button*/}
@@ -61,8 +60,8 @@ export default function NewNavbar() {
               <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
                 <div className="flex justify-center md:w-auto flex-shrink-0 items-center font-bold w-full">
                   <img
-                    className="h-10 flex items-center justify-center"
-                    src="./img/whiteDelTech.png"
+                    className="h-12 flex items-center justify-center"
+                    src="/img/whiteDelTech.png"
                   ></img>
                 </div>
                 <div className="flex justify-center items-center w-full">
@@ -119,7 +118,7 @@ export default function NewNavbar() {
                                 "block px-4 py-2 text-sm text-gray-700"
                               )}
                             >
-                              Manage(beta)
+                              Manage(alpha)
                             </a>
                           )}
                         </Menu.Item>
@@ -133,6 +132,32 @@ export default function NewNavbar() {
                               )}
                             >
                               CreateBlog
+                            </a>
+                          )}
+                        </Menu.Item>
+                        <Menu.Item>
+                          {({ active }) => (
+                            <a
+                              href="/register/delegateRegistration"
+                              className={classNames(
+                                active ? "bg-gray-100" : "",
+                                "block px-4 py-2 text-sm text-gray-700"
+                              )}
+                            >
+                              Delegate Register
+                            </a>
+                          )}
+                        </Menu.Item>
+                        <Menu.Item>
+                          {({ active }) => (
+                            <a
+                              href="/register/ipRegistration"
+                              className={classNames(
+                                active ? "bg-gray-100" : "",
+                                "block px-4 py-2 text-sm text-gray-700"
+                              )}
+                            >
+                              IP Register
                             </a>
                           )}
                         </Menu.Item>
@@ -154,7 +179,10 @@ export default function NewNavbar() {
                   </Menu>
                 ) : (
                   <button onClick={() => signIn()}>
-                    <div className="outline rounded p-1 hover:outline-none hover:bg-white ">
+                    <div
+                      className="py-3 px-4 w-fit duration-500 text-[#1341EC] border-2 border-[#1341EC] rounded-xl
+            hover:bg-gradient-to-t from-[#1341EC] to-[#142e8a] hover:text-[#fff] "
+                    >
                       Login
                     </div>
                   </button>
