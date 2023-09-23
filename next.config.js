5079c69d0769032be42bf63687c906ec6e9fcd54
Vaibhav <vaibhav2438@gmail.com>
2023-09-23 17:34:01 +0530

    bugs remove

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    serverActions: true,
-  },
-};
+// const nextConfig = {
+//   experimental: {
+//     serverActions: true,
+//   },
+// };
 
 module.exports = {
-  nextConfig,
+  // nextConfig,
   images: {
     formats: ["image/webp"],
     remotePatterns: [
