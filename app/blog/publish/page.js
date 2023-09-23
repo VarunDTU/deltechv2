e52d9ed080c70e034664e2f11536224fa774879a
Vaibhav <vaibhav2438@gmail.com>
2023-09-23 17:38:30 +0530

    getpost removed

--- app/blog/publish/page.js
+++ app/blog/publish/page.js
@@ -1,16 +1,16 @@
-// "use client";
-// import dynamic from "next/dynamic";
-// import { CreateBlog } from "./uploadBlog";
-// import { useSession } from "next-auth/react";
-// export default function Page() {
-//   const { data: session } = useSession({
-//     required: true,
-//   });
-//   const Jodit = dynamic(() => import("./editor"), { ssr: false });
+"use client";
+import dynamic from "next/dynamic";
+import { CreateBlog } from "./uploadBlog";
+import { useSession } from "next-auth/react";
+export default function Page() {
+  const { data: session } = useSession({
+    required: true,
+  });
+  const Jodit = dynamic(() => import("./editor"), { ssr: false });
 
-//   return (
-//     <div>
-//       <Jodit></Jodit>
-//     </div>
-//   );
-// };
+  return (
+    <div>
+      <Jodit></Jodit>
+    </div>
+  );
+};
