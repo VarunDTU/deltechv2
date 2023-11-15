9b17655b723d6076808cd426672f5eba2817fb13
Vaibhav <vaibhav2438@gmail.com>
2023-11-15 18:16:02 +0530

    important updates section added

--- app/Updates/Update.js
+++ app/Updates/Update.js
@@ -1,35 +1,35 @@
 "use client"
 
-import React from "react";
-import SectionTitle from "../components/sectionTitle";
+// import React from "react";
+// import SectionTitle from "../components/sectionTitle";
 
 const ImportantUpdate = () => {
   const updates = [
     {
       id: "update-1",
-      content: "Registrations now open.",
+      content: "The distribution of email addresses to the campus ambassador delegates will commence shortly.",
     },
     {
       id: "update-1",
-      content: "If you’re unable to attend the meet due to any reason, mail the reason with proof of absenteeism to deltechmun.memberaffairs@gmail.com",
+      content: "Registrations are now open.",
     },
-    {
-      id: "update-1",
-      content: "If you’re unable to attend the meet due to any reason, mail the reason with proof of absenteeism to deltechmun.memberaffairs@gmail.com",
-    },
-    {
-      id:"update-1",
-      content:
-        "The training session will be conducted tomorrow (28 October, Saturday) at 12pm in online mode.Reiterating, it is mandatory for all to attend, the very basic structure and RoPs would be discussed in tomorrow’s training session.",
-    },
+    // {
+    //   id: "update-1",
+    //   content: "If you’re unable to attend the meet due to any reason, mail the reason with proof of absenteeism to deltechmun.memberaffairs@gmail.com",
+    // },
+    // {
+    //   id:"update-1",
+    //   content:
+    //     "The training session will be conducted tomorrow (28 October, Saturday) at 12pm in online mode.Reiterating, it is mandatory for all to attend, the very basic structure and RoPs would be discussed in tomorrow’s training session.",
+    // },
   ];
 
   return (
     <>
-      <SectionTitle
+      {/* <SectionTitle
         title="Updates"
         excerpt="Find all the important updates related to the society here."
-      />
+      /> */}
       <div className="h-80 bg-[#040b23] bg my-10 py-5 text-gray-300 mx-auto overflow-y-scroll no-scrollbar max-w-4xl rounded-3xl font-merriweather">
         <h1 className="text-center text-[36px] font-bold tracking-wider mb-4">Important Updates</h1>
         <hr className="border-gray-400 mx-6"/>
--- app/page.js
+++ app/page.js
@@ -2,12 +2,14 @@ import HeroSection from "./homepage/herosection";
 import CommitteeSection from "./homepage/committeeSection";
 import About from "./homepage/about";
 import Venue from "./homepage/venue";
+import ImportantUpdate from "./Updates/Update";
 
 export default function Page() {
   return (
     <>
       <HeroSection />
       <About />
+      <ImportantUpdate />
       <CommitteeSection />
       <Venue />
     </>
