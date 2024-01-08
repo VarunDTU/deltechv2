95f3f46fa7154ab79f2f633ce3edafad249a0cf6
Vaibhav <vaibhav2438@gmail.com>
2024-01-08 15:59:05 +0530

    Update page.js

--- app/blog/[slug]/page.js
+++ app/blog/[slug]/page.js
@@ -15,27 +15,27 @@ export default async function Page({ params }) {
             className="self-center flex-shrink-0 w-24 h-24 border rounded-full md:justify-self-start dark:bg-gray-500 dark:border-gray-700"
           />
           <div className="px-5">
-            <h4 className="text-lg font-semibold">{blog?.author?.name}</h4>
+            <h4 className="text-lg font-semibold font-merriweather tracking-wide">{blog?.author?.name}</h4>
             <p className="dark:text-gray-400">{blog?.author?.bio}</p>
           </div>
         </div>
         <div className="flex flex-col items-center mx-auto pt-10 rounded">
-          <div className="p-6 pb-12 m-4 mx-auto -mt-16 space-y-6 lg:max-w-2xl sm:px-10 sm:mx-12 lg:rounded-md dark:bg-gray-800">
-            <div className="space-y-2">
+          <div className="p-6 pb-12 m-4 mx-auto -mt-16 space-y-6 lg:max-w-3xl sm:px-10 sm:mx-12 lg:rounded-md dark:bg-gray-800">
+            <div className="space-y-2 flex flex-col items-center justify-center">
               <a
                 rel="noopener noreferrer"
                 href="#"
-                className="inline-block text-4xl text-center font-semibold sm:text-3xl pb-5"
+                className="inline-block text-xl font-merriweather tracking-wider font-semibold sm:text-2xl pb-5"
               >
                 {blog?.title}
               </a>
               <img
                 src={blog?.thumbnail.url}
                 alt=""
-                className="w-full h-60 sm:h-96 dark:bg-gray-500 rounded"
+                className="h-60 sm:h-96 self-center dark:bg-gray-500 rounded"
               />
             </div>
-            <div className="text-gray-100">
+            <div className="text-gray-100 font-sans">
               <EditorJsRenderer data={blog?.description} className="text-sm" />
             </div>
           </div>
