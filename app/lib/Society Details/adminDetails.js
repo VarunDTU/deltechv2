09df7ba27b81b1b0f2049a5f2dfd50c3a1989bcd
Vaibhav <vaibhav2438@gmail.com>
2024-01-13 23:24:02 +0530

    Update adminDetails.js

--- app/lib/Society Details/adminDetails.js
+++ app/lib/Society Details/adminDetails.js
@@ -1,1 +1,1 @@
-export default ["vaibhav2438@gmail.com", "zostera2438@gmail.com", "evambansal@gmail.com"];
+export default ["vaibhav2438@gmail.com", "vaibhavgupta_ep21b11_50@dtu.ac.in", "evambansal@gmail.com"];
