0fca847a7a4470857e9511818ed3b30cdbacb2c0
Vaibhav <vaibhav2438@gmail.com>
2024-01-14 21:47:56 +0530

    blog page text color change

--- app/components/richTextEditor/editorJsRenderer.js
+++ app/components/richTextEditor/editorJsRenderer.js
@@ -9,7 +9,7 @@ const EditorJsRenderer = ({ data }) => {
   const html = EditorJsToHtml.parse(data);
   return (
     <>
-      <div className="prose max-w-full" key={data.time}>
+      <div className="prose max-w-full text-gray-100" key={data.time}>
         {html.map((item, index) => {
           if (typeof item === "string") {
             return (
